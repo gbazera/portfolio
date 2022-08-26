46561655631589c64d57e197d24791b4e7159962
gbazera <bazerashvili.gio27@gmail.com>
2022-08-27 00:31:10 +0400

    started making website responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,23 @@
+import { useRef, useState } from "react";
+
 function Header() {
+    const [burgerActive, setBurgerActive] = useState(false);
+    const [navActive, setNavActive] = useState(false);
+    
+    const toggleMenu=()=>{
+        setBurgerActive(!burgerActive);
+        setNavActive(!navActive);
+    }
+
     return(
         <nav className="container">
             <a className="logo" href="/#">giorgi bazerashvili</a>
-            <ul className="nav-links">
+            <ul className={`nav-links ${navActive ? 'active' : null}`}>
                 <li><a href="/#">home</a></li>
                 <li><a href="/#projects">projects</a></li>
                 <li><a href="/#contact">contact</a></li>
             </ul>
+            <button className="burger" onClick={toggleMenu}><i className={`bx ${burgerActive ? 'bx-x' : 'bx-menu'}`}></i></button>
         </nav>
     );
 }
